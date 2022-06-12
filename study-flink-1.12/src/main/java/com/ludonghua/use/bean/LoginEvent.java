01df1ce41a0e88f6de6c51309f5459a15ed1ff0a
scludonghua <scludonghua@126.com>
2022-06-12 12:24:28 +0800

    状态编程和CEP

---
+++ study-flink-1.12/src/main/java/com/ludonghua/use/bean/LoginEvent.java
@@ -0,0 +1,20 @@
+package com.ludonghua.use.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Author Luis
+ * DATE 2022-06-09 23:42
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginEvent {
+    private Long userId;
+    private String ip;
+    private String eventType;
+    private Long eventTime;
+}
+
